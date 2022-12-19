5d748af48520630af89994b46f63bf65ac0bcc38
tangweiye <1010565305@.com>
2022-12-19 22:07:23 +0800

    can divide diff types of questions but still need to improve

--- src/main/java/GrabDatas.java
+++ src/main/java/GrabDatas.java
@@ -1,24 +1,61 @@
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.regex.*;
 
-import static org.apache.commons.lang3.math.NumberUtils.isNumber;
-
 public class GrabDatas {
     private Document doc;
-    private HashMap<String, List<String>> map=new HashMap<>();
+    private List<Integer> countTypeQ;
+    private List<List<String>>answerSet;
 
-    public void getDatas(Web web){
-        doc=web.getDocument();
-        Elements elms=doc.getElementsByTag("strong");
-        String regex=".*[?]\t*?";
-        Pattern pattern=Pattern.compile(regex);
-        for (Element elm: elms){
-            Matcher matcher=pattern.matcher(elm.text());
-            if (matcher.matches()) System.out.println(elm.text());
+    public void getDatas(Web web) {
+        countTypeQ = new ArrayList<>();
+        doc = web.getDocument();
+        Elements elms = doc.getElementsByTag("strong");
+        Elements elmsAus = doc.getElementsByTag("li");
+        String regexQues = ".*[?].*?";
+        String regexMulti2choices = ".*\\(Choose two.\\)";
+        String regexMulti3choices = ".*\\(Choose three.\\)";
+        Pattern pattern = Pattern.compile(regexQues);
+        for (Element elm : elms) {
+            //load all questions from web and match
+            Matcher matcher = pattern.matcher(elm.text());
+            //simple choice
+            if (matcher.matches()) {
+//                System.out.println(elm.text());
+                countTypeQ.add(1);
+            }
+            //2choices
+            if (Pattern.matches(regexMulti2choices, elm.text())) {
+                countTypeQ.add(2);
+            }
+            //3choices
+            if (Pattern.matches(regexMulti3choices, elm.text())) {
+                countTypeQ.add(3);
+            }
+        }
+        //start to iterate ans
+        int i=0;
+        answerSet=new ArrayList<>();
+        for (Element elm : elmsAus) {
+            int count=countTypeQ.get(i);
+            Elements listElm = elm.getElementsByTag("strong");
+            //store multiple choices ans
+            List<String>curAns=new ArrayList<>();
+            for (Element iterator : listElm) {
+//                System.out.println(iterator.text());
+                curAns.add(iterator.text());
+                count--;
+                if(count==0){
+                    answerSet.add(curAns);
+                    i++;
+                }
+            }
         }
     }
 }
+
