045f2d765dba2dc751c204379916e6d6e243e659
tangweiye <1010565305@.com>
2022-12-19 18:58:29 +0800

    have grabed Qs

--- src/main/java/GrabDatas.java
+++ src/main/java/GrabDatas.java
@@ -1,18 +1,24 @@
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-
 import java.util.HashMap;
+import java.util.List;
+import java.util.regex.*;
+
+import static org.apache.commons.lang3.math.NumberUtils.isNumber;
 
 public class GrabDatas {
     private Document doc;
-    private HashMap<String,String> map=new HashMap<>();
+    private HashMap<String, List<String>> map=new HashMap<>();
 
     public void getDatas(Web web){
         doc=web.getDocument();
         Elements elms=doc.getElementsByTag("strong");
+        String regex=".*[?]\t*?";
+        Pattern pattern=Pattern.compile(regex);
         for (Element elm: elms){
-            System.out.println(elm.text());
+            Matcher matcher=pattern.matcher(elm.text());
+            if (matcher.matches()) System.out.println(elm.text());
         }
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-
+        Web web=new Web("https://itexamanswers.net/ccna-1-v7-modules-14-15-network-application-communications-" +
+                "exam-answers.html");
+        GrabDatas data=new GrabDatas();
+        data.getDatas(web);
     }
 }
