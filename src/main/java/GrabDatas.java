7815c2fbd419699ba1d0764fda89d2c50d140155
tangweiye <1010565305@.com>
2022-12-25 16:31:19 +0800

    can divide diff types of questions but still need to improve

--- src/main/java/GrabDatas.java
+++ src/main/java/GrabDatas.java
@@ -10,10 +10,12 @@ import java.util.regex.*;
 public class GrabDatas {
     private Document doc;
     private List<Integer> countTypeQ;
-    private List<List<String>>answerSet;
+    private List<List<String>> answerSet;
+    private List<String> questions;
 
     public void getDatas(Web web) {
         countTypeQ = new ArrayList<>();
+        questions = new ArrayList<>();
         doc = web.getDocument();
         Elements elms = doc.getElementsByTag("strong");
         Elements elmsAus = doc.getElementsByTag("li");
@@ -28,14 +30,17 @@ public class GrabDatas {
             if (matcher.matches()) {
 //                System.out.println(elm.text());
                 countTypeQ.add(1);
+                questions.add(elm.text());
             }
             //2choices
             if (Pattern.matches(regexMulti2choices, elm.text())) {
                 countTypeQ.add(2);
+                questions.add(elm.text());
             }
             //3choices
             if (Pattern.matches(regexMulti3choices, elm.text())) {
                 countTypeQ.add(3);
+                questions.add(elm.text());
             }
         }
         //start to iterate ans
@@ -45,15 +50,26 @@ public class GrabDatas {
             int count=countTypeQ.get(i);
             Elements listElm = elm.getElementsByTag("strong");
             //store multiple choices ans
-            List<String>curAns=new ArrayList<>();
+//            List<String>curAns=new ArrayList<>();
             for (Element iterator : listElm) {
-//                System.out.println(iterator.text());
-                curAns.add(iterator.text());
-                count--;
-                if(count==0){
-                    answerSet.add(curAns);
-                    i++;
-                }
+                System.out.println(iterator.text());
+//                curAns.add(iterator.text());
+//                count--;
+//                if(count==0){
+//                    answerSet.add(curAns);
+//                    i++;
+//                }
+            }
+        }
+    }
+
+    public void listAnswers(Web web) {
+        doc = web.getDocument();
+        Elements elmsAus = doc.getElementsByTag("li");
+        for (Element elm : elmsAus) {
+            Elements listElm = elm.getElementsByTag("strong");
+            for (Element iterator : listElm) {
+                System.out.println(iterator.text());
             }
         }
     }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        Web web=new Web("https://itexamanswers.net/ccna-1-v7-modules-14-15-network-application-communications-" +
+        Web web = new Web("https://itexamanswers.net/ccna-1-v7-modules-14-15-network-application-communications-" +
                 "exam-answers.html");
-        GrabDatas data=new GrabDatas();
-        data.getDatas(web);
+        GrabDatas data = new GrabDatas();
+        data.listAnswers(web);
+//        data.getDatas(web);
     }
 }
